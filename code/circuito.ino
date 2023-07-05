f6fad22acf3cd2a474b92b637e4355b84ece1fa6
EM40a <hermosilla.emanuel811@gmail.com>
2023-07-05 19:28:10 -0300

    Correcion de errores

--- code/circuito.ino
+++ code/circuito.ino
@@ -9,13 +9,10 @@
 #define CONTROL_IR 11
 #define SERVOMOTOR 9
 
-#define limiteTemperatura 50 // El limite de temperatura limite antes de que se active la alarma
+#define limiteTemperatura 50 
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2); 
-
 IRrecv irRecibidor(CONTROL_IR); // Indicamos que pin recibirá los datos
-decode_results irDato; // Guarda los datos recibidos
-
 Servo servoMotor; 
 
 // Variables globales
@@ -27,11 +24,7 @@ void setup()
 {
   pinMode(LED_1, OUTPUT);
   pinMode(LED_2, OUTPUT);
-
-  int temperaturaLeida = analogRead(SENSOR_TEMPERATURA); // Recibe un valor de 0 a 1023
-  // Mapea el ese valor a un rango de -40 a 125, que son los limites del sensor de temperatura
-  temperatura = map(temperaturaLeida, 20, 350, -40, 125); 
-
+  
   lcd.begin(16, 2); // Inicializa el LCD con 16 columnas y 2 filas
   servoMotor.attach(SERVOMOTOR); // Inicializa el ServoMotor
   irRecibidor.begin(CONTROL_IR, DISABLE_LED_FEEDBACK); // Inicializa el Control IR
@@ -39,17 +32,25 @@ void setup()
 
 void loop()
 {
+  int temperaturaLeida = analogRead(SENSOR_TEMPERATURA); // Recibe un valor de 0 a 1023
+  // Mapea el ese valor a un rango de -40 a 125, que son los limites del sensor de temperatura
+  temperatura = map(temperaturaLeida, 20, 350, -40, 125); 
+
   ControlRemotoIR(); // Controla el sistema con el control remoto  
-  EstacionAnio(temperatura); // Indica la estacion del año segun la temperatura
 
   if (encendido) {
-    AlarmaIncendio(temperatura);
-    ImprimirTemperatura(temperatura);
-    digitalWrite(LED_1, HIGH);
+    EjecutarPrograma(temperatura);
   } else {
     digitalWrite(LED_1, LOW);
     lcd.clear(); // Limpiamos el LCD
   }
+}
+
+void EjecutarPrograma(int temp){
+  EstacionAnio(temp); // Indica la estacion del año segun la temperatura
+  AlarmaIncendio(temp);
+  ImprimirTemperatura(temp);
+  digitalWrite(LED_1, HIGH);
 }
 
 void ControlRemotoIR()
@@ -59,21 +60,13 @@ void ControlRemotoIR()
       case 0xFF00BF00: // Boton de encendido
         encendido = !encendido;
         break;
-
-      case 0xF50ABF00: //Boton de subir
-        temperatura += 5;
-        break;
-
-      case 0xF708BF00: //Boton de Bajar
-        temperatura -= 5;
-        break;
-
+      // ... Proximas Funcionalidades 
       default:
         break;
     }
     irRecibidor.resume(); // Recibe el siguiente dato
   }
-  delay(100); // Evitar rebotes
+  delay(120); // Evitar rebotes
 }
 
 void EstacionAnio(int temp)
@@ -87,7 +80,7 @@ void EstacionAnio(int temp)
   } else if (temp >= -5 and temp < 10){
     estacion = "Invierno";
   }
-  else {
+  else{
     estacion = "";
   }
 }
@@ -95,13 +88,10 @@ void EstacionAnio(int temp)
 void ImprimirTemperatura(int temp)
 {
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
-  lcd.print(temp);
-  lcd.print("C");
+  lcd.print("Temp: "+ (String) temp + "C");
   lcd.setCursor(0, 1);
   lcd.print(estacion);
-  delay(600);
+  delay(200);
 }
 
 // Prende un led y apaga el otro
@@ -126,8 +116,9 @@ void EncenderServo(int tiempo)
 void AlarmaIncendio(int temp)
 {
   if (temp >= limiteTemperatura) {
-    lcd.clear();
+    lcd.setCursor(0, 1);
     lcd.print("Incendio!!!");
     EncenderServo(600);
+    lcd.clear();
   }
 }
